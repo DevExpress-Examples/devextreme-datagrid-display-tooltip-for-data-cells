237535cb6c245ad9a5d41d971d9b0c0dba1b97a5
Charles Vincent Cordial (DevExpress) <charles.cordial@devsoft.ph>
2025-08-13 12:50:47 +0800

    Fix: jQUery implementation

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,10 +1,62 @@
 $(() => {
-  let count = 0;
-  $('#btn').dxButton({
-    text: `Click count: ${count}`,
-    onClick(e) {
-      count += 1;
-      e.component.option('text', `Click count: ${count}`);
-    },
+  $("#gridContainer").dxDataGrid({
+    dataSource: employees,
+    keyExpr: "ID",
+    showBorders: true,
+    width: 500,
+    columns: ["FirstName", "LastName", "BirthDate"],
+    onCellPrepared: function (e) {
+      if (e.rowType === "data" && e.column.dataField === "FirstName") {
+        e.cellElement.mouseover(function (arg) {
+          tooltipInstance.option("contentTemplate", function (contentElement) {
+            contentElement.html(
+              `<div class='tooltipContent'>
+                 <div><b>Position:</b> ${e.data.Position}</div>
+                 <div><b>State:</b> ${e.data.State}</div>
+               </div>`
+            );
+          });
+          tooltipInstance.show(arg.target);
+        });
+
+        e.cellElement.mouseout(function () {
+          tooltipInstance.hide();
+        });
+      }
+    }
   });
+
+  var tooltipInstance = $("#tooltipContainer").dxTooltip({
+    position: "right"
+  }).dxTooltip("instance");
 });
+
+var employees = [
+  {
+    "ID": 1,
+    "Prefix": "Mr.",
+    "FirstName": "John",
+    "LastName": "Heart",
+    "Position": "CEO",
+    "State": "California",
+    "BirthDate": "1964/03/16"
+  },
+  {
+    "ID": 2,
+    "Prefix": "Mrs.",
+    "FirstName": "Olivia",
+    "LastName": "Peyton",
+    "Position": "Sales Assistant",
+    "State": "California",
+    "BirthDate": "1981/06/03"
+  },
+  {
+    "ID": 3,
+    "Prefix": "Mr.",
+    "FirstName": "Robert",
+    "LastName": "Reagan",
+    "Position": "CMO",
+    "State": "Arkansas",
+    "BirthDate": "1974/09/07"
+  }
+];
