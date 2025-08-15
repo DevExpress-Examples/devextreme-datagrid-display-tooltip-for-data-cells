70d848bb946231ab06e9e15a1f867a9ab3d3060f
Charles Vincent Cordial (DevExpress) <charles.cordial@devsoft.ph>
2025-08-15 12:33:17 +0800

    Fix: Angular implementation, lint, and build issues

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { DxButtonModule } from 'devextreme-angular/ui/button';
+import { DxDataGridModule } from 'devextreme-angular/ui/data-grid';
+import { DxTooltipModule } from 'devextreme-angular/ui/tooltip';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -11,7 +12,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DxButtonModule,
+    DxDataGridModule,
+    DxTooltipModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
