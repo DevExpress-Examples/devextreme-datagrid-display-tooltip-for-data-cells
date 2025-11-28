02c80d52e7f0e1feecc61331eaa97fe07bf7f886
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2025-11-28 14:11:06 +0200

    Fix parsing errors of tsconfig.json

--- Vue/.eslintrc.cjs
+++ Vue/.eslintrc.cjs
@@ -1,22 +1,23 @@
 module.exports = {
   root: true,
-  extends: ["devextreme/spell-check"],
-  overrides: [
-    {
-      files: ["*.ts", "*.vue"],
-      extends: ["@vue/eslint-config-typescript", "devextreme/vue"],
-      env: { es6: true },
-      parserOptions: {
-        project: "./tsconfig.json",
-        createDefaultProgram: true,
-        // eslint-disable-next-line spellcheck/spell-checker
-        ecmaVersion: 2020,
-      },
-      globals: {
-        System: false,
-        AzureGateway: false,
-        AzureFileSystem: false,
-      },
+  extends: ['devextreme/spell-check'],
+  overrides: [{
+    files: ['*.ts', '*.vue'],
+    extends: [
+      '@vue/eslint-config-typescript',
+      'devextreme/vue'
+    ],
+    env: { es6: true },
+    parserOptions: {
+      project: './tsconfig.json',
+      tsconfigRootDir: __dirname,
+      'createDefaultProgram': true,
+      'ecmaVersion': 2020,
     },
-  ],
+    globals: {
+      System: false,
+      AzureGateway: false,
+      AzureFileSystem: false,
+    },
+  }]
 };
